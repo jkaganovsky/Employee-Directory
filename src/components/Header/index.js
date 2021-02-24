39323f94aeeb6667fd80746dabb2a34af8558f01
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-23 21:54:39 -0800

    Added starting codes and files for components and utils folders, including App.js and index.js.

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">Search by name or sort by category</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
