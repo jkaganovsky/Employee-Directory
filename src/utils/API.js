c8f9cc96bff029008ee8e7165735063ade7612ac
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-25 21:48:00 -0800

    Added Random User Generator API call.

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,11 +4,7 @@ import axios from "axios";
 export default {
 
   getRandomUser: function() {
-    return axios.get("https://randomuser.me/api/");
+    return axios.get("https://randomuser.me/api/?results=50");
   }
-
-  // getName: function() {
-  //   return axios.get("https://randomuser.me/api/?inc=name")
-  // }
 
 };
