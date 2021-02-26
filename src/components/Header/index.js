235a7102ba61d75a67499eedf12b134c86139a24
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-25 22:22:44 -0800

    Added text-center to h1 and p tags.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,8 @@ function Header() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h1 className="display-4">Employee Directory</h1>
-        <p className="lead">Search by name or sort by category</p>
+        <h1 className="display-4 text-center">Employee Directory</h1>
+        <p className="lead text-center">Search by name or sort by category</p>
       </div>
     </div>
   )
