8f7bef7abcd78331f3b3535bb6a8864062f90337
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-28 23:42:54 -0800

    Added container div, rearranged input formatting, and added name value to input.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,10 +2,19 @@ import MaterialIcon from 'material-icons-react';
 
 function Search(props) {
   return (
-    <form className="form-inline my-2 my-lg-0 justify-content-end">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={props.employeeSearch} onChange={props.nameSearch} />
-      <MaterialIcon icon="search" />
-    </form>
+    <div className="container">
+      <form className="form-inline my-2 my-lg-0 justify-content-end">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          name="search"
+          value={props.employeeSearch}
+          onChange={props.nameSearch} />
+        <MaterialIcon icon="search" />
+      </form>
+    </div>
   )
 }
 
