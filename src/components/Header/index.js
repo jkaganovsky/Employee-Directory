5d4c8188bcefc463b2e2eaffbf8a543c1e83b4bf
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-28 23:41:13 -0800

    Added container div.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 function Header() {
   return (
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4 text-center">Employee Directory</h1>
-        <p className="lead text-center">Search by name or sort by category</p>
+    <div className="container">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center">Employee Directory</h1>
+          <p className="lead text-center">Search by name or sort by category</p>
+        </div>
       </div>
     </div>
   )
