91cc6653a627bf055756b9282eb40cd4b5bc453b
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-02 00:30:31 -0800

    Changed title to sort and search by first name.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header() {
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="display-4 text-center">Employee Directory</h1>
-          <p className="lead text-center">Search by name or sort by category</p>
+          <p className="lead text-center">Search or sort by first name</p>
         </div>
       </div>
     </div>
