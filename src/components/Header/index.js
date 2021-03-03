44d7951417bcfac5b6ad92126af68cbbf52a684f
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-02 22:01:35 -0800

    Changed header p tag to add additional sort and search feature.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,8 @@ function Header() {
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="display-4 text-center">Employee Directory</h1>
-          <p className="lead text-center">Search or sort by first name</p>
+          <p className="lead text-center">Search by First, Last, Email or Phone</p>
+          <p className="lead text-center">or Sort by First or Email </p>
         </div>
       </div>
     </div>
